2f7412ad691874999ab3dcb572dd197d4b542f3c
HaQi <luqmanananta745@gmail.com>
2024-10-03 16:53:12 +0700

    pb3a

---
+++ pertemuan-4/operator.php
@@ -0,0 +1,19 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a /$b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil operator <br>";
+echo "Hasil Tambah: {$hasilTambah} <br>";
+echo "Hasil Kurang: {$hasilKurang} <br>";
+echo "Hasil Kali: {$hasilKali} <br>";
+echo "Hasil Bagi: {$hasilBagi} <br>";
+echo "Hasil Sisa Bagi: {$sisaBagi} <br>";
+echo "Hasil Pangkat: {$pangkat} <br>";
+?>
