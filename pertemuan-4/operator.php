c24dcf29c35f5120f74a8714a924143d1d6d44ab
HaQi <luqmanananta745@gmail.com>
2024-10-03 17:01:20 +0700

    pb3c

--- pertemuan-4/operator.php
+++ pertemuan-4/operator.php
@@ -32,4 +32,14 @@ echo "Hasil Lebih Besar: {$hasilLebihBesar} <br>";
 echo "Hasil Lebih Kecil Sama: {$hasilLebihKecilSama} <br>";
 echo "Hasil Lebih Besar Sama: {$hasilLebihBesarSama} <br>";
 echo"<br>";
+
+$hasilAnd = $a && $b;
+$hasilOr = $a || $b;
+$hasilNotA = !$a;
+$hasilNotB = !$b;
+
+echo"Hasil And: {$hasilAnd} <br>";
+echo"Hasil Or: {$hasilOr} <br>";
+echo"Hasil Not A: {$hasilNotA} <br>";
+echo"Hasil Not B: {$hasilNotB} <br>";
 ?>
