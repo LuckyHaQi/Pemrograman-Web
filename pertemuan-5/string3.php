05afe35b81a52dd93ec4caeb93dc3b61bfe5c883
HaQi <luqmanananta745@gmail.com>
2024-10-03 21:54:08 +0700

    pb4c

---
+++ pertemuan-5/string3.php
@@ -0,0 +1,14 @@
+<?php
+$pesan1 ="ayaS arek malang";
+echo strrev($pesan1) . "<br>";
+
+$pesan= "ayaS arek malang";
+# ubah variabel $pesan menjadi array dengan perintah explode
+$pesanPerKata = explode(" ", $pesan);
+# ubah setiap kata dalam array menjadi kebalikannya
+$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+# gabungkan kembali array menjadi string
+$pesan = implode(" ", $pesanPerKata);
+
+echo $pesan. "<br>";
+?>
