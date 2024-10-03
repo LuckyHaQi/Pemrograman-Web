e758d25dc8d3d858337d1078a7e6fe6d97281ecc
HaQi <luqmanananta745@gmail.com>
2024-10-03 16:27:15 +0700

    pb1c

--- pertemuan-4/variabel_konstanta.php
+++ pertemuan-4/variabel_konstanta.php
@@ -7,4 +7,10 @@ echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil";
 $benar = true;
 $salah = false;
 echo "Variabel benar: $benar, Variabel salah: $salah";
+
+// Mendefinisikan konstanta untuk nilai tetap
+define("NAMA_SITUS", "WebsiteKu.com");
+define("TAHUN_PENDIRIAN", 2003);
+
+echo "Selamat datang di " . NAMA_SITUS . ", situs yang didirikan pada tahun " . TAHUN_PENDIRIAN . ".";
 ?>
