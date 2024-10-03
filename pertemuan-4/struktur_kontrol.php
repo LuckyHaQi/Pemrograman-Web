790c6f6283f4fd2ba0992d7fa6ea39006bc42528
HaQi <luqmanananta745@gmail.com>
2024-10-03 18:31:42 +0700

    pb4e

--- pertemuan-4/struktur_kontrol.php
+++ pertemuan-4/struktur_kontrol.php
@@ -44,4 +44,14 @@ foreach ($skorUjian as $skor) {
 
 echo"<br>";
 echo "Total skor ujian adalah: $totalSkor";
+
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai <60) {
+        echo "Nilai: $nilai (Tidak lulus)<br>";
+        continue;
+    }
+    echo "Nilai: $nilai (lulus) <br>";
+}
 ?>
